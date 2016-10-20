4651814aa5fafebe11d3f3372376a1782f82f7ed
Daisuke Fujita <dtanshi45@gmail.com>
2016-10-21 01:02:55 +0900

    Add s3url recipe

---
+++ Formula/s3url.rb
@@ -0,0 +1,14 @@
+class S3url < Formula
+  desc "Generate S3 object pre-signed URL in one command"
+  homepage "https://github.com/dtan4/s3url"
+  url "https://github.com/dtan4/s3url/releases/download/v0.3.1/s3url-v0.3.1-darwin-amd64.tar.gz"
+  sha256 "cabdcea887ec566c00d3a615d8439e299a32f12e70b3a64f1d2a702ac3ca0c59"
+
+  def install
+    bin.install "s3url"
+  end
+
+  test do
+    system "s3url", "-v"
+  end
+end
