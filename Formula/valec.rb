c0d2670f5444d31a2502a4ceec6b855418f6cc7f
Daisuke Fujita <dtanshi45@gmail.com>
2016-12-06 17:10:50 +0900

    Add Valec formula

---
+++ Formula/valec.rb
@@ -0,0 +1,14 @@
+class Valec < Formula
+  desc "Handle application secrets securely"
+  homepage "https://github.com/dtan4/valec"
+  url "https://github.com/dtan4/valec/releases/download/v0.2.1/valec-v0.2.1-darwin-amd64.tar.gz"
+  sha256 "11c92e70fbebbf24809d8986ac635e09a0fd9843bdaac8b0fabd5444eb4fea8c"
+
+  def install
+    bin.install "valec"
+  end
+
+  test do
+    system "valec", "version"
+  end
+end
