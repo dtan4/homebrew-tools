135b8ac8dbedc077e22b573a282d75b16a2ea927
Daisuke Fujita <dtanshi45@gmail.com>
2016-12-15 17:30:50 +0900

    Update Valec to v0.3.1

--- Formula/valec.rb
+++ Formula/valec.rb
@@ -1,8 +1,8 @@
 class Valec < Formula
   desc "Handle application secrets securely"
   homepage "https://github.com/dtan4/valec"
-  url "https://github.com/dtan4/valec/releases/download/v0.2.1/valec-v0.2.1-darwin-amd64.tar.gz"
-  sha256 "11c92e70fbebbf24809d8986ac635e09a0fd9843bdaac8b0fabd5444eb4fea8c"
+  url "https://github.com/dtan4/valec/releases/download/v0.3.1/valec-v0.3.1-darwin-amd64.tar.gz"
+  sha256 "d9d060d81a62643b5acc727216373afa7e3d5d83faf9a9e770421c651c7307a7"
 
   def install
     bin.install "valec"
