b6e112fa14426680c20d35574faefec168784142
Daisuke Fujita <dtanshi45@gmail.com>
2016-12-15 17:35:46 +0900

    Modify checksum of Valec v0.3.1

--- Formula/valec.rb
+++ Formula/valec.rb
@@ -2,7 +2,7 @@ class Valec < Formula
   desc "Handle application secrets securely"
   homepage "https://github.com/dtan4/valec"
   url "https://github.com/dtan4/valec/releases/download/v0.3.1/valec-v0.3.1-darwin-amd64.tar.gz"
-  sha256 "d9d060d81a62643b5acc727216373afa7e3d5d83faf9a9e770421c651c7307a7"
+  sha256 "c81a5f49f23fe5be19e8078feba9a6e8cb1e5220ad8100585e61b6acc94b1c80"
 
   def install
     bin.install "valec"
