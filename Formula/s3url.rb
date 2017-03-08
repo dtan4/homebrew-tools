5a63c7f46bed290f8cceee378b7aee5c2233700d
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-08 22:17:23 +0900

    Upgrade s3url to v1.0.0

--- Formula/s3url.rb
+++ Formula/s3url.rb
@@ -1,11 +1,11 @@
 class S3url < Formula
-  VERSION = "0.3.1".freeze
+  VERSION = "1.0.0".freeze
 
   desc "Generate S3 object pre-signed URL in one command"
   homepage "https://github.com/dtan4/s3url"
   url "https://github.com/dtan4/s3url/releases/download/v#{VERSION}/s3url-v#{VERSION}-darwin-amd64.tar.gz"
   version VERSION
-  sha256 "cabdcea887ec566c00d3a615d8439e299a32f12e70b3a64f1d2a702ac3ca0c59"
+  sha256 "402a40859ae3be3d42ea69aaa85c7b2b048c4662a20db13c8351759526dc707b"
 
   def install
     bin.install "s3url"
