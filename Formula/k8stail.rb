00aa036e929f471e77a0be5a8237246506bd2394
Daisuke Fujita <dtanshi45@gmail.com>
2017-04-11 14:18:17 +0900

    Upgrade k8stail to v0.4.0

--- Formula/k8stail.rb
+++ Formula/k8stail.rb
@@ -1,11 +1,11 @@
 class K8stail < Formula
-  VERSION = "0.3.0".freeze
+  VERSION = "0.4.0".freeze
 
   desc "`tail -f` experience for Kubernetes Pods"
   homepage "https://github.com/dtan4/k8stail"
   url "https://github.com/dtan4/k8stail/releases/download/v#{VERSION}/k8stail-v#{VERSION}-darwin-amd64.tar.gz"
   version VERSION
-  sha256 "d293ab88f7785764f8dec161e6b673549b9a9a2017f912ac57aeddddf4f85238"
+  sha256 "0395b9223f52d5c5fde7048f451e95e88a6818bc0c16ada9f8f22be5af6fd893"
 
   def install
     bin.install "k8stail"
