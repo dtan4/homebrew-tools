0e387dcf0b65216b9bd8f02f04acb736f41e17d8
Daisuke Fujita <dtanshi45@gmail.com>
2017-08-16 10:14:06 +0900

    Upgrade k8sec to v0.5.0

--- Formula/k8sec.rb
+++ Formula/k8sec.rb
@@ -1,11 +1,11 @@
 class K8sec < Formula
-  VERSION = "0.3.1".freeze
+  VERSION = "0.5.0".freeze
 
   desc "CLI tool to manage Kubernetes Secrets easily"
   homepage "https://github.com/dtan4/k8sec"
   url "https://github.com/dtan4/k8sec/releases/download/v#{VERSION}/k8sec-v#{VERSION}-darwin-amd64.tar.gz"
   version VERSION
-  sha256 "7398649043e7a9518ef4865401a78c09805b67f138cb37d729ebb1c230385050"
+  sha256 "d8a897842a7eb7a0f99331545fda621e7d3db01287e5ea0bab6981ff62214be9"
 
   def install
     bin.install "k8sec"
