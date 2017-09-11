f06ce67e2bea63dfe9540e72054c71732a01e1e1
koudaiii <cs006061@gmail.com>
2017-09-11 17:40:12 +0900

    Bump up to v0.5.1 k8stail

--- Formula/k8stail.rb
+++ Formula/k8stail.rb
@@ -1,11 +1,11 @@
 class K8stail < Formula
-  VERSION = "0.4.0".freeze
+  VERSION = "0.5.1".freeze
 
   desc "`tail -f` experience for Kubernetes Pods"
   homepage "https://github.com/dtan4/k8stail"
   url "https://github.com/dtan4/k8stail/releases/download/v#{VERSION}/k8stail-v#{VERSION}-darwin-amd64.tar.gz"
   version VERSION
-  sha256 "0395b9223f52d5c5fde7048f451e95e88a6818bc0c16ada9f8f22be5af6fd893"
+  sha256 "66cda739f7014ee976a92ba4d32f14956936b5bcd02192dc1e906592167bd714"
 
   def install
     bin.install "k8stail"
