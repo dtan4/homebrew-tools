5365487fb81eee9f9fd0c1d02f9039347f6279c1
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-31 17:01:22 +0900

    Brew formula update for s3url version v1.0.1

--- Formula/s3url.rb
+++ Formula/s3url.rb
@@ -2,24 +2,24 @@
 class S3url < Formula
   desc "Generate S3 object pre-signed URL in one command"
   homepage "https://github.com/dtan4/s3url"
-  version "1.0.1-rc5"
+  version "1.0.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/dtan4/s3url/releases/download/v1.0.1-rc5/s3url_Darwin_x86_64.tar.gz"
-    sha256 "8aa6df2889502da673712b0054f62b1339ec1c0d66f8a57b9cc242063f95d0fb"
+    url "https://github.com/dtan4/s3url/releases/download/v1.0.1/s3url_Darwin_x86_64.tar.gz"
+    sha256 "c31f6748417d0c772ee0b2a9868da3bf160622a87e82465e5a1038701f3535ca"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/dtan4/s3url/releases/download/v1.0.1-rc5/s3url_Linux_x86_64.tar.gz"
-      sha256 "8ef6b0cf37c1ac4cd22777f0e222fe38d36dc6876ec1975a131f0a9f5f6dc098"
+      url "https://github.com/dtan4/s3url/releases/download/v1.0.1/s3url_Linux_x86_64.tar.gz"
+      sha256 "bd597777e58f62722fa372f803c9df5f34a347de35dcc0c39206e5bd39b94369"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/dtan4/s3url/releases/download/v1.0.1-rc5/s3url_Linux_arm64.tar.gz"
-        sha256 "57f3ab130df14a433cb7b8d07d309ef14c462a274cd153d9550c7d3efaec9bbb"
+        url "https://github.com/dtan4/s3url/releases/download/v1.0.1/s3url_Linux_arm64.tar.gz"
+        sha256 "d39f1142c2b9ce6e93ad7edc125eec3ed13924475de2666efdfa9b7eff93f7e2"
       else
-        url "https://github.com/dtan4/s3url/releases/download/v1.0.1-rc5/s3url_Linux_armv6.tar.gz"
-        sha256 "01c1189a55d0dfe6415ac7561ee09934692c17f0eadd8ecb4501e573918326d6"
+        url "https://github.com/dtan4/s3url/releases/download/v1.0.1/s3url_Linux_armv6.tar.gz"
+        sha256 "96a349b2f3c81ee9b6904aae468496fe4875a9b4f8c48c321040dcd2c1cf6317"
       end
     end
   end
